2f4723d040f90039bb68c1df8d5c432401d3c166
JeremyHerpe <herpe.jeremy@gmail.com>
2023-06-28 16:57:50 +0200

    modifs back + Router front

--- Backend/database.sql
+++ Backend/database.sql
@@ -4,7 +4,15 @@ CREATE TABLE
         username VARCHAR(100) NOT NULL,
         email VARCHAR(100) NOT NULL,
         hashedPassword varchar(255) NOT NULL,
-        firstname VARCHAR(100) NOT NULL,
-        lastname VARCHAR(100) NOT NULL,
         is_admin TINYINT DEFAULT 0
+    );
+
+    CREATE TABLE
+    `price`(
+        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        cat_premium INTEGER NOT NULL,
+        cat_a INTEGER NOT NULL,
+        cat_b INTEGER NOT NULL,
+        cat_c INTEGER NOT NULL,
+        cat_d INTEGER NOT NULL,
     );
