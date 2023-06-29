65994c77fc9862a4eeccb27f35d980c74e4377fc
JeremyHerpe <herpe.jeremy@gmail.com>
2023-06-29 22:28:21 +0200

    JWT OK Front

--- Frontend/src/03.globals/03.Components/Valider.jsx
+++ Frontend/src/03.globals/03.Components/Valider.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-handleClick = (e) => {
-    setPrice(e.target.value)
-}
+// handleClick = (e) => {
+//     setPrice(e.target.value)
+// }
 
 function Valider() {
     return (
