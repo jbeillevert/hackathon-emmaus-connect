560da0d6e2a1013167692442850fa747179aee0b
JeremyHerpe <herpe.jeremy@gmail.com>
2023-06-30 00:08:06 +0200

    corrections

--- Frontend/src/03.globals/03.Components/MenuDashboard.jsx
+++ Frontend/src/03.globals/03.Components/MenuDashboard.jsx
@@ -4,9 +4,6 @@ import { LoginContext } from '../../02.navigation/LoginContext';
 
 const MenuDashboard = () => {
 
-    const normalLink = 'py-3 pl-6 rounded-tl-lg rounded-bl-lg flex text-stone-200 translate-x-4'
-    const activeLink = 'py-3 pl-6 rounded-tl-lg rounded-bl-lg flex bg-stone-200 text-teal-900 translate-x-4'
-
     const { loggedIn, setLoggedIn, logout } = useContext(LoginContext);
 
     const handleLogout = () => {
@@ -22,18 +19,13 @@ const MenuDashboard = () => {
         }
     }, [logout]);
 
+    const normalLink = 'py-3 pl-6 rounded-tl-lg rounded-bl-lg flex text-stone-200 translate-x-4'
+    const activeLink = 'py-3 pl-6 rounded-tl-lg rounded-bl-lg flex bg-stone-200 text-teal-900 translate-x-4'
 
     return (
         <div className="flex justify-start h-screen top-0 left-0 bg-gradient-to-br from-teal-900 to-teal-700 w-[200px] fixed p-3">
             <div id="menudashboxcontent" className="flex flex-col h-full items-start justify-between">
-                <div className="">
-                    <NavLink to="/" className="py-3 pl-6 rounded-tl-lg rounded-bl-lg flex">
-                        <h4 className="font-semibold text-lg text-stone-200">Simulateur</h4>
-                    </NavLink>
-                    <NavLink to="/dashboard/faq" className="py-3 pl-6 rounded-tl-lg rounded-bl-lg flex">
-                        <h4 className="font-semibold text-lg text-stone-200">Fonctionnement</h4>
-                    </NavLink>
-                </div>
+
                 <div id='menudashboxcontent' className='flex flex-col h-full items-start justify-between'>
                     <div className=''>
                         <img src="../src/01.assets/logo.png" alt="logo Emmaus" className='scale-75' />
