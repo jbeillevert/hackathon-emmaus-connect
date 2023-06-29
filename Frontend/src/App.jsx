ff77deb8b2e9f8165695db8b2c783559e6bf4072
Thibault BENOIT <thibaultbenoit0@gmail.com>
2023-06-29 14:24:29 +0200

    composant estimation ok

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Simulator from "./03.globals/01.Pages/Simulator"
+import Estimation from "./03.globals/03.Components/Estimation"
 
 function App() {
 
 
   return (
     <div className="bg-stone-200 h-full">
+
     <Simulator />
 
     </div>
