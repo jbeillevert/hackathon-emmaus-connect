e5c2a0a1afdf4754b2f282302a2a7d7c7fdafc35
J-E <beillevertj.contact@gmail.com>
2023-06-29 17:14:55 +0200

    renvoi de la bonne valeur dans la popup

--- Frontend/src/03.globals/03.Components/Estimation.jsx
+++ Frontend/src/03.globals/03.Components/Estimation.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 
-const Estimation = ({estimateOpen, setEstimateOpen}) => {
+const Estimation = ({estimateOpen, setEstimateOpen, brand, model, resultAlgo}) => {
 
     const estimateCloseHandler = () => {
         setEstimateOpen(!estimateOpen);
     }
     return (
         estimateOpen && 
-            (<div  className='flex flex-col justify-center gap-10 items-center -translate-x-2/4 -translate-y-2/4 z-10 shadow-md bg-white rounded-md absolute inset-y-1/2 inset-x-2/4 w-3/12 h-60 p-10'>
-            <h2 className='sukui-h2'>Estimation :</h2>
-            <h3 className='sukui-h3'> CATEGORIE D</h3>
-            <h6>Prix suggéré : de € à €</h6>
-            <a href="" className='absolute bottom-3 right-3 text-teal-700 text-xs hover:text-teal-400'>Télécharger en pdf</a>
+            (<div  className=' -translate-x-2/4 -translate-y-2/4 z-10 shadow-md bg-white rounded-md absolute inset-y-1/4 gap-6 flex flex-col justify-center inset-x-2/4 w-[600px] h-72 p-10'>
+                <div>
+                    <p className='sukui-p'>Estimation :</p>
+                </div>
+                <div className='flex flex-col'>
+                    <h4 className='sukui-h4'>{brand} {model}</h4>
+                    <h3 className='sukui-h3'>{resultAlgo}</h3>
+                    {/* <p className='sukui-p'>Prix suggéré : de € à €</p> */}
+                </div>
+                <div>
+                    <a href="" className=' text-teal-700 text-sm border-b border-solid hover:text-teal-400'>Télécharger en pdf</a>
+                </div>
             <p className='sukui-p absolute top-3 right-4 hover: cursor-pointer hover:text-neutral-950' onClick={estimateCloseHandler}>X</p>
         </div>)
     );
