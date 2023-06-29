7f32b7e540e8d531dd6757be38e16ee569872165
JeremyHerpe <herpe.jeremy@gmail.com>
2023-06-29 23:44:35 +0200

    jwt OK, front quasi OK

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,35 @@
-import React from "react"
-
+import React, { useState, useMemo } from "react"
+import { LoginContext } from "./02.navigation/LoginContext"
 import Router from "./02.navigation/Router"
 import MenuDashboard from "./03.globals/03.Components/MenuDashboard"
 
-
-
-
-
-
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const login = () => {
+    setLoggedIn(true);
+  };
+  const logout = () => {
+    setLoggedIn(false);
+    localStorage.removeItem("token");
+  };
+  const loginContextValue = useMemo(
+    () => ({
+      loggedIn,
+      setLoggedIn,
+      login,
+      logout,
+    }),
+    [loggedIn, setLoggedIn]
+  );
+
   return (
 
-
     <div className="bg-stone-200 min-h-screen flex">
-      <MenuDashboard />
-      <Router />
+      <LoginContext.Provider value={loginContextValue}>
+        <MenuDashboard />
+        <Router />
+      </LoginContext.Provider>
     </div>
   )
 };
